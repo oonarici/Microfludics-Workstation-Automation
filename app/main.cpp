825b73650a7409bfefa1f7c7cb632f251a9eba2c
oonarici <onuronarici@gmail.com>
2026-02-03 10:00:31 +0300

    update CI workflow

---
+++ app/main.cpp
@@ -0,0 +1,12 @@
+#include <QApplication>
+#include <QWidget>
+
+int main(int argc, char** argv)
+{
+QApplication app(argc, argv);
+QWidget window;
+window.setWindowTitle("Device Control");
+window.resize(800, 600);
+window.show();
+return app.exec();
+}
---
+++ tests/test_smoke.cpp
@@ -0,0 +1,7 @@
+#include <cassert>
+
+int main()
+{
+assert(1 + 1 == 2);
+return 0;
+}
